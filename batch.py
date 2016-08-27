ced9ec121a9b57c8f0dc6e53c672e5232f7cc122
César <cesarhernandez@campusciff.net>
2016-08-27 13:47:22 +0200

    Quito tildes en batch.py con hora cambiada

--- batch.py
+++ batch.py
@@ -158,11 +158,11 @@ def actualiza_calidad_aire():
 #Hay que poner 2 horas menos de las que son en realidad debido a problemas en heroku de horas
 #scheduler.add_job(timed_job, 'interval', seconds=5)
 #realmente se ejecuta a las 13:42
-scheduler.add_job(envioMail, 'cron', day_of_week='mon-sun', hour=11, minute=42)
+scheduler.add_job(envioMail, 'cron', day_of_week='mon-sun', hour=11, minute=50)
 
 
 #realmente se ejecuta a las 13:42
-scheduler.add_job(actualiza_calidad_aire, 'cron', day_of_week='mon-sun', hour=11, minute=42)
+scheduler.add_job(actualiza_calidad_aire, 'cron', day_of_week='mon-sun', hour=11, minute=50)
 #realmente se ejecuta a las 22:35
 scheduler.add_job(actualiza_calidad_aire, 'cron', day_of_week='mon-sun', hour=20, minute=35)
 scheduler.start()

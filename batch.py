eeaabe0a6b8e2b00cd9a78a15724dff64fe82416
César <cesarhernandez@campusciff.net>
2016-08-30 20:42:05 +0200

    Subida 30082016

--- batch.py
+++ batch.py
@@ -203,22 +203,132 @@ def noticias_del_dia():
     db.noticias_del_dia.insert_many(recordsdf)
     conexion.close()  
 	
+def prediccionesAEMET():
+    import urllib
+    import xmltodict
+    from bs4 import BeautifulSoup
+
+    url = "http://www.aemet.es/es/eltiempo/prediccion/municipios?p=28&w=t"
+    f = urllib.urlopen(url)
+    myfile = f.read()
+    soup = BeautifulSoup(myfile)
+
+    #Conectamos a la base de datos
+    import base64
+    import json
+    from pymongo import MongoClient as Connection
+    cadenaCon= 'mongodb://othesoluciones:'+base64.b64decode("b3RoZXNvbHVjaW9uZXM=")+'@ds029635.mlab.com:29635/othesoluciones1'
+    MONGODB_URI =cadenaCon
+    conexion = Connection(MONGODB_URI)
+    db = conexion.othesoluciones1
+    db.prediccionesAEMET.drop()
+	
+    for urls in soup.find_all('td'):
+        localidad = urls.a.text
+        url = "http://www.aemet.es"+urls.a['href']
+
+        print localidad
+        r = urllib.urlopen(url).read()
+
+        soup = BeautifulSoup(r)
+
+        #Enlace a los xml con las predicciones
+
+        xmlLink = soup.find_all('div', class_="enlace_xml")
+
+        for xml in xmlLink:
+            xmlUrl= "http://www.aemet.es"+xml.a['href'] 
+        #prediccionAEMET (xmlUrl)
+        file = urllib.urlopen(xmlUrl)
+        data = file.read()
+        file.close()
+
+        data = xmltodict.parse(data)
+
+        db.prediccionesAEMET.insert_one(data)
+    conexion.close()  	
+
+	
+def NivelesPolenMadrid():
+    import pandas as pd
+    import numpy as np
+    import urllib
+    from bs4 import BeautifulSoup
+
+     #Conectamos a la base de datos
+    import base64
+    import json
+    from pymongo import MongoClient as Connection
+    cadenaCon= 'mongodb://othesoluciones:'+base64.b64decode("b3RoZXNvbHVjaW9uZXM=")+'@ds029635.mlab.com:29635/othesoluciones1'
+    MONGODB_URI =cadenaCon
+    conexion = Connection(MONGODB_URI)
+    db = conexion.othesoluciones1
+
+    link ="http://polenes.com/graficos/jsp/ImgGrafico.jsp?chkSelTodos=on&chkPolenes=CUPRE&chkPolenes=PALMA&chkPolenes=RUMEX&chkPolenes=MERCU&chkPolenes=MORUS&chkPolenes=URTIC&chkPolenes=ALNUS&chkPolenes=BETUL&chkPolenes=CAREX&chkPolenes=FRAXI&chkPolenes=QUERC&chkPolenes=OLEA&chkPolenes=PINUS&chkPolenes=ULMUS&chkPolenes=CASTA&chkPolenes=POPUL&chkPolenes=GRAMI&chkPolenes=QUEAM&chkPolenes=PLATA&chkPolenes=PLANT&chkPolenes=ARTEM&chkPolenes=ALTER&selEstacion=MAD&selAnioTrimes=&selPeriodo=USE&txtFDesde=01%2F01%2F2010&txtFHasta=12%2F08%2F2016&prov=MAD&hidPolen=%26polen%3DCUPRE%26polen%3DPALMA%26polen%3DRUMEX%26polen%3DMERCU%26polen%3DMORUS%26polen%3DURTIC%26polen%3DALNUS%26polen%3DBETUL%26polen%3DCAREX%26polen%3DFRAXI%26polen%3DQUERC%26polen%3DOLEA%26polen%3DPINUS%26polen%3DULMUS%26polen%3DCASTA%26polen%3DPOPUL%26polen%3DGRAMI%26polen%3DQUEAM%26polen%3DPLATA%26polen%3DPLANT%26polen%3DARTEM%26polen%3DALTER&hidCheckSel=&mostrarGraf=S&hidPolenSolo=N&idio=ES&numPolenesSeleccionados=22&hidAniosEstacion=&primero=trueutm_source=twitter&utm_medium=twitter&utm_campaign=twitter"
+    f = urllib.urlopen(link)
+    myfile = f.read()
+
+    soup = BeautifulSoup(myfile)
+
+    tabla = soup.findAll("table")[1]
+    datos = []
+
+    for dato in tabla.find_all("td"):
+        datos.append(dato.get_text().strip())
+
+    datos.pop(0)
+    valores = [s for s in datos if s.isdigit()]
+
+    #Calculamos la posicion del primer valor numerico
+    ind =0
+    for s in datos:
+        ind+=1
+        if s.isdigit() and primerValor==0:
+            primerValor=ind
+
+    columnas =[]
+    for i in range(primerValor-2):
+        if len(datos[i].split("\n"))>1:
+            columnas.append(datos[i].split("\n")[1])
+        else:
+            columnas.append(datos[i])
+
+    tamColumnas = len(columnas)
+    k=primerValor-2
+    lista = []
+    while k<len(datos):
+        lista.append(datos[k:k+tamColumnas])
+        k+=tamColumnas
+
+    df=pd.DataFrame(lista,columns=columnas)
+    recordsdf = json.loads(df.T.to_json()).values()
+    db.noticias_del_dia.insert_many(recordsdf)
+    conexion.close()  
+
 	
 #Hay que poner 2 horas menos de las que son en realidad debido a problemas en heroku de horas
 #scheduler.add_job(timed_job, 'interval', seconds=5)
 
-#realmente se ejecuta a las 13:42
-scheduler.add_job(envioMail, 'cron', day_of_week='mon-sun', hour=12, minute=10)
+#realmente se ejecuta a las 08:15
+scheduler.add_job(envioMail, 'cron', day_of_week='mon-sun', hour=06, minute=15)
+
+#realmente se ejecuta a las 08:30
+scheduler.add_job(actualiza_calidad_aire, 'cron', day_of_week='mon-sun', hour=06, minute=30)
+
+#realmente se ejecuta a las 08:45
+scheduler.add_job(prediccionesAEMET, 'cron', day_of_week='mon-sun', hour=06, minute=45)
+
+#realmente se ejecuta a las 09:00
+scheduler.add_job(NivelesPolenMadrid, 'cron', day_of_week='mon-sun', hour=06, minute=45)
+
+#realmente se ejecuta a las 09:30
+scheduler.add_job(noticias_del_dia, 'cron', day_of_week='mon-sun', hour=07, minute=15)
+
+#realmente se ejecuta a las 20:30
+scheduler.add_job(actualiza_calidad_aire, 'cron', day_of_week='mon-sun', hour=18, minute=30)
 
 
-#realmente se ejecuta a las 13:42
-scheduler.add_job(actualiza_calidad_aire, 'cron', day_of_week='mon-sun', hour=11, minute=50)
 
-#realmente se ejecuta a las 22:35
-scheduler.add_job(actualiza_calidad_aire, 'cron', day_of_week='mon-sun', hour=20, minute=35)
-
-#realmente se ejecuta a las 22:35
-scheduler.add_job(noticias_del_dia, 'cron', day_of_week='mon-sun', hour=20, minute=35)
 
 scheduler.start()
 

94dbd88fb27b32c3e3f11d3075eaa6a908297013
othesoluciones <othesoluciones@gmail.com>
2016-09-05 12:11:01 +0200

    1013

--- batch.py
+++ batch.py
@@ -4,6 +4,7 @@ import base64
 import smtplib
 from apscheduler.schedulers.blocking import BlockingScheduler
 import logging
+import datetime
 import unicodedata
 
 logging.basicConfig()
@@ -369,7 +370,7 @@ def NivelesPolenMadrid():
 #scheduler.add_job(actualiza_calidad_aire, 'cron', day_of_week='mon-sun', hour=06, minute=27)
 
 #realmente se ejecuta a las 08:45
-scheduler.add_job(prediccionesAEMET, 'cron', day_of_week='mon-sun', hour=10, minute=10)
+scheduler.add_job(prediccionesAEMET, 'cron', day_of_week='mon-sun', hour=10, minute=13)
 
 #realmente se ejecuta a las 09:00
 #scheduler.add_job(NivelesPolenMadrid, 'cron', day_of_week='mon-sun', hour=06, minute=40)

a22e85063eb6d96c3d58e760b763f38d55b32c6c
othesoluciones <othesoluciones@gmail.com>
2016-09-07 10:02:22 +0200

    cambio horas

--- batch.py
+++ batch.py
@@ -387,13 +387,13 @@ scheduler.add_job(envioMail, 'cron', day_of_week='mon-sun', hour=06, minute=47)
 scheduler.add_job(actualiza_calidad_aire, 'cron', day_of_week='mon-sun', hour=06, minute=49)
 
 #realmente se ejecuta a las 08:45
-scheduler.add_job(prediccionesAEMET, 'cron', day_of_week='mon-sun', hour=08, minute=02)
+scheduler.add_job(prediccionesAEMET, 'cron', day_of_week='mon-sun', hour=08, minute=5)
 
 #realmente se ejecuta a las 09:00
 scheduler.add_job(NivelesPolenMadrid, 'cron', day_of_week='mon-sun', hour=06, minute=59)
 
 #realmente se ejecuta a las 09:30
-scheduler.add_job(noticias_del_dia, 'cron', day_of_week='mon-sun', hour=08, minute=01)
+scheduler.add_job(noticias_del_dia, 'cron', day_of_week='mon-sun', hour=08, minute=4)
 
 #realmente se ejecuta a las 20:30
 scheduler.add_job(actualiza_calidad_aire, 'cron', day_of_week='mon-sun', hour=18, minute=30)

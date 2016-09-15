bf806fed65f51b3c84a9af430f4f9e828e3a944d
othesoluciones <othesoluciones@gmail.com>
2016-09-15 12:50:47 +0200

    para ejecutar todos con manzanaresEl Real

--- batch.py
+++ batch.py
@@ -712,21 +712,21 @@ def algoritmoPredictivo():
 scheduler.add_job(envioMail, 'cron', day_of_week='mon-sun', hour=7, minute=23)
 
 #realmente se ejecuta a las 09:10
-scheduler.add_job(noticias_del_dia, 'cron', day_of_week='mon-sun', hour=6, minute=38)
+scheduler.add_job(noticias_del_dia, 'cron', day_of_week='mon-sun', hour=10, minute=53)
 
 #realmente se ejecuta a las 08:46
-scheduler.add_job(actualiza_calidad_aire, 'cron', day_of_week='mon-sun', hour=10, minute=21)
+scheduler.add_job(actualiza_calidad_aire, 'cron', day_of_week='mon-sun', hour=10, minute=54)
 
 #realmente se ejecuta a las 08:47. Este tarda
-scheduler.add_job(prediccionesAEMET, 'cron', day_of_week='mon-sun', hour=10, minute=22)
+scheduler.add_job(prediccionesAEMET, 'cron', day_of_week='mon-sun', hour=10, minute=55)
 
 #realmente se ejecuta a las 08:55
-scheduler.add_job(NivelesPolenMadrid, 'cron', day_of_week='mon-sun', hour=10, minute=28)
+scheduler.add_job(NivelesPolenMadrid, 'cron', day_of_week='mon-sun', hour=11, minute=01)
 
 
 
 #realmente se ejecuta a las 09:12
-scheduler.add_job(algoritmoPredictivo, 'cron', day_of_week='mon-sun', hour=10, minute=29)
+scheduler.add_job(algoritmoPredictivo, 'cron', day_of_week='mon-sun', hour=11, minute=02)
 #realmente se ejecuta a las 20:30
 #scheduler.add_job(actualiza_calidad_aire, 'cron', day_of_week='mon-sun', hour=18, minute=30)
 

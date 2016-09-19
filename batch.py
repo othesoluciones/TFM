ca23b6f52348e7b645cb1644515d64a9d79c9fdc
othesoluciones <othesoluciones@gmail.com>
2016-09-19 09:02:12 +0200

    Update batch.py

--- batch.py
+++ batch.py
@@ -978,13 +978,13 @@ scheduler.add_job(actualiza_calidad_aire, 'cron', day_of_week='mon-sun', hour=6,
 scheduler.add_job(NivelesPolenMadrid, 'cron', day_of_week='mon-sun', hour=7, minute=02)
 
 #realmente se ejecuta a las 09:10
-scheduler.add_job(noticias_del_dia, 'cron', day_of_week='mon-sun', hour=7, minute=05)
+scheduler.add_job(noticias_del_dia, 'cron', day_of_week='mon-sun', hour=7, minute=06)
 
 #realmente se ejecuta a las 09:12
-scheduler.add_job(algoritmoPredictivo, 'cron', day_of_week='mon-sun', hour=7, minute=08)
+scheduler.add_job(algoritmoPredictivo, 'cron', day_of_week='mon-sun', hour=7, minute=10)
 
 #realmente se ejecuta a las 08:45
-scheduler.add_job(envioMail, 'cron', day_of_week='mon-sun', hour=7, minute=09)
+scheduler.add_job(envioMail, 'cron', day_of_week='mon-sun', hour=7, minute=13)
 #realmente se ejecuta a las 20:30
 #scheduler.add_job(actualiza_calidad_aire, 'cron', day_of_week='mon-sun', hour=18, minute=30)
 
